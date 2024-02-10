6773c6cefdc65ea480ed946c59786544689ffa4a
@7Karina <karinafrank750@gmail.com>
2024-02-10 13:33:14 +0200

    create index.js

--- src/contacts.js
+++ src/contacts.js
@@ -1,46 +1,83 @@
 const fs = require('node:fs/promises');
 const path = require('node:path');
+const crypto = require('crypto');
 
-const contactsPath = path.join('src', 'db', 'contacts.json');
+const contactsPath = path.join(__dirname, 'db', 'contacts.json');
 
 async function readContacts() {
-  const data = await fs.readFile(contactsPath, { encoding: 'utf-8' });
-
-  return JSON.parse(data);
+  try {
+    const data = await fs.readFile(contactsPath, { encoding: 'utf-8' });
+    return JSON.parse(data);
+  } catch (error) {
+    console.error('Error reading contacts:', error);
+    throw error;
+  }
 }
 
 async function writeContacts(contacts) {
-  return fs.writeFile(contactsPath, contacts);
+  try {
+    const data = JSON.stringify(contacts, null, 2);
+    await fs.writeFile(contactsPath, data);
+  } catch (error) {
+    console.error('Error writing contacts:', error);
+    throw error;
+  }
 }
 
 async function listContacts() {
-  const contacts = await readContacts();
-  return contacts;
+  try {
+    const contacts = await readContacts();
+    return contacts;
+  } catch (error) {
+    console.error('Error listing contacts:', error);
+    throw error;
+  }
 }
 
 async function getContactById(contactId) {
-  const contacts = await readContacts();
-
-  const contact = contacts.find(contact => contact.id === contactId);
-  return contact || null;
+  try {
+    const contacts = await readContacts();
+    const contact = contacts.find(contact => contact.id === contactId);
+    return contact || null;
+  } catch (error) {
+    console.error('Error getting contact by ID:', error);
+    throw error;
+  }
 }
 
 async function removeContact(contactId) {
-  const contacts = await readContacts();
-  const index = contacts.findIndex(contact => contact.id === contactId);
-  if (index !== -1) {
-    const removedContact = contacts.splice(index, 1);
-    return removedContact[0];
-  } else {
-    return null;
+  try {
+    const contacts = await readContacts();
+    const index = contacts.findIndex(contact => contact.id === contactId);
+    if (index !== -1) {
+      const removedContact = contacts.splice(index, 1);
+      await writeContacts(contacts);
+      return removedContact[0];
+    } else {
+      return null;
+    }
+  } catch (error) {
+    console.error('Error removing contact:', error);
+    throw error;
   }
 }
 
 async function addContact(name, email, phone) {
-  // ...твій код. Повертає об'єкт доданого контакту (з id).
+  try {
+    const contacts = await readContacts();
+    const newContact = { id: crypto.randomUUID(), name, email, phone };
+    contacts.push(newContact);
+    await writeContacts(contacts);
+    return newContact;
+  } catch (error) {
+    console.error('Error adding contact:', error);
+    throw error;
+  }
 }
 
 module.exports = {
-  readContacts,
-  writeContacts,
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
 };
--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
-import { program } from 'commander';
+const contacts = require('./contacts');
+const { Command } = require('commander');
+
+const program = new Command();
 program
   .option('-a, --action <type>', 'choose action')
   .option('-i, --id <type>', 'user id')
@@ -6,27 +9,31 @@ program
   .option('-e, --email <type>', 'user email')
   .option('-p, --phone <type>', 'user phone');
 
-program.parse();
+program.parse(process.argv);
 
-const options = program.opts();
+const argv = program.opts();
 
 // TODO: рефакторити
 async function invokeAction({ action, id, name, email, phone }) {
   switch (action) {
     case 'list':
-      // ...
+      allContacts = await contacts.listContacts();
+      console.table(allContacts);
       break;
 
     case 'get':
-      // ... id
+      oneContact = await contacts.getContactById(id);
+      console.table(oneContact);
       break;
 
     case 'add':
-      // ... name email phone
+      newContact = await contacts.addContact({ name, email, phone });
+      console.table(newContact);
       break;
 
     case 'remove':
-      // ... id
+      deleteContact = await contacts.removeContact(id);
+      console.table(deleteContact);
       break;
 
     default:
@@ -34,4 +41,4 @@ async function invokeAction({ action, id, name, email, phone }) {
   }
 }
 
-invokeAction(options);
+invokeAction(argv);
